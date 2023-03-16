61ac95c65dd2981441b924c43b190744334e6850
loicbaecher <74916339+loicbaecher@users.noreplay.github.com>
2023-03-16 21:20:03 +0100

    add layout and basic functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+//Math functions:
+
+function add(x,y){
+    return(x+y);
+}
+
+function substract(x,y){
+    return(x-y);
+}
+
+function multiply(x,y){
+    return(x*y);
+}
+
+function divide(x,y){
+    return(x/y);
+}
+
+function operate(x,y,choos){
+    return(choos(x,y));
+}
