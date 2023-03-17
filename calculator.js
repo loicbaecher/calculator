7127e270b5ce6c44e065fb427eaedef442176c7e
loicbaecher <74916339+loicbaecher@users.noreplay.github.com>
2023-03-17 19:27:03 +0100

    almost done. problems with divide by zero
    Other checks are also necessary

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
 //Math functions:
 
 function add(x,y){
+    x=parseInt(x);
+    y=parseInt(y);
     return(x+y);
 }
 
@@ -13,11 +15,16 @@ function multiply(x,y){
 }
 
 function divide(x,y){
-    return(x/y);
+    if(y=="0"){
+        return("ERROR")
+    }
+    else{
+        return(Math.round((x/y)*100)/100);
+    }
 }
 
-function operate(x,y,choos){
-    return(choos(x,y));
+function operate(x,y,ope){
+    return(ope(x,y));
 }
 
 //add clear function
@@ -38,25 +45,68 @@ buttons.forEach((button)=>{
 
 //When clicking on an operator, store the first number
 opers=document.querySelectorAll("button#operator");
+screen=document.querySelector("div.screen");
 opers=Array.from(opers);
+let store=[];
+let ope="";
 opers.forEach((oper)=>{
     oper.addEventListener('click',function(e){
-        if(store!=[]){
+        if(store.length==0){
+            store.push(screen.textContent);
+        }
+        currentnumber=[];
+        if(ope==""){
+            ope=e.target.getAttribute("class");
+        }
+        else{
+            switch(ope){
+                case "add":screen.textContent=operate(store,screen.textContent,add) 
+                break;
+                case "substract":screen.textContent=operate(store,screen.textContent,substract)
+                break;
+                case "multiply":screen.textContent=operate(store,screen.textContent,multiply)
+                break;
+                case "divide":screen.textContent=operate(store,screen.textContent,divide)
+                console.log(operate(store,screen.textContent,divide));
+                    if((operate(store,screen.textContent,divide))==NaN){
+                        return("ERROR");
+                    }
+                    else{
+                        screen.textContent=operate(store,screen.textContent,divide);
+                    }
+                break;
+            };
             store=screen.textContent;
+            ope=e.target.getAttribute("class");
         }
-        else(){
-            
-        }
-        store=screen.textContent;
-        ope=
     })
+})
+
+equal=document.querySelector("button.equal")
+equal.addEventListener('click',function(e){
+    switch(ope){
+        case "add":screen.textContent=operate(store,screen.textContent,add) 
+        break;
+        case "substract":screen.textContent=operate(store,screen.textContent,substract)
+        break;
+        case "multiply":screen.textContent=operate(store,screen.textContent,multiply)
+        break;
+        case "divide":
+            if(operate(store,screen.textContent,divide)=="ERROR"){
+                return("ERROR");
+            }
+            else{
+                screen.textContent=operate(store,screen.textContent,divide);
+            }
+        break;
+    };
 })
 
 //Add the number to the display when pressing a key
 buttons=document.querySelectorAll("button#number");
 buttons=Array.from(buttons);
 screen=document.querySelector("div.screen");
-let currentnumber=[];
+var currentnumber=[];
 buttons.forEach((button)=>{
     button.addEventListener('click',function(e){
         currentnumber.push(e.target.textContent)
@@ -64,7 +114,7 @@ buttons.forEach((button)=>{
             currentnumber=[]
         }
         else{
-        screen.textContent=currentnumber.join(",").replaceAll(","," ");
+        screen.textContent=currentnumber.join(",").replaceAll(",","");
         }
     })
 })
@@ -74,5 +124,7 @@ clear=document.querySelector("button.clear");
 clear.addEventListener('click',function(e){
     screen.textContent=0;
     currentnumber=[];
+    store=[];
+    ope="";
 })
 
